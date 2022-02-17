d5126496e95dcff9da17e46b21adb865cfafaffe
Amir Helmy Shawky <helmyamir06@gmail.com>
2022-02-17 20:48:40 +0200

    Time: 1045 ms (36.32%), Space: 21.3 MB (83.69%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.py
@@ -0,0 +1,44 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        arr = nums
+        if len(arr) > 1:
+
+             # Finding the mid of the array
+            mid = len(arr)//2
+
+            # Dividing the array elements
+            L = arr[:mid]
+
+            # into 2 halves
+            R = arr[mid:]
+
+            # Sorting the first half
+            Solution.sortArray(self,L)
+
+            # Sorting the second half
+            Solution.sortArray(self,R)
+
+            i = j = k = 0
+
+            # Copy data to temp arrays L[] and R[]
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+
+            # Checking if any element was left
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+           
+        return arr
